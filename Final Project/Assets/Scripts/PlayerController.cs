4984922582f791a9852fe25ea840e289759642a6
Kassidy1027 <KNC23@pct.edu>
2024-03-29 19:08:42 -0400

    Updated PlayerController Script

--- Final Project/Assets/Scripts/PlayerController.cs
+++ Final Project/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,17 @@ public class PlayerController : MonoBehaviour
      * VARIABLE/OBJECT DECLARATION
      */
 
+    // PUBLIC VARIABLES
+
+    // Variable to Hold the Player's Speed 
+    public int speed;
+
+    // Variable to get the Player's Facing Direction 
+    public Vector2 facingDirection = Vector2.right;
+
+
+    // PRIVATE VARIABLES
+
     // Variable to get the Player's Movement 
     private float horizontal;
 
@@ -39,7 +50,61 @@ public class PlayerController : MonoBehaviour
         // Getting the Player's Horizontal Movement 
         horizontal = Input.GetAxis("Horizontal");
 
+        // Checking if Player is Moving
+        if (horizontal > 0 | horizontal < 0)
+        {
+            // Dealing with Player Movement and Sprite Direction 
+            if (horizontal > 0 && facingDirection == Vector2.left)
+            {
+                // Setting the isWalking Bool to True
+                anim.SetBool("isWalking", true);
+
+                // Chaning the Scale to Rotate Everything on Player
+                FlipX();
+
+                // Flipping the Vector for Bullet Firing
+                facingDirection = Vector2.right;
+
+            }
+            else if (horizontal < 0 && facingDirection == Vector2.right)
+            {
+                // Setting the isWalking Bool to True
+                anim.SetBool("isWalking", true);
+
+                // Chaning the Scale to Rotate Everything on Player
+                FlipX();
+
+                // Flipping the Vector for Bullet Firing
+                facingDirection = Vector2.left;
+
+            } // END OF IF/ELSEIF
+
+            // Updating the Player's Transform
+            transform.Translate(new Vector3(horizontal, 0, 0) * speed * Time.deltaTime);
+
+        }
+        // Player Not Moving
+        else 
+        {
+            // Setting the isWalking Bool to False
+            anim.SetBool("isWalking", false);
+
+        } // END OF IF/ELSE
 
 
     } // END OF METHOD 
-}
+
+
+    /*
+     * EXTRA/CUSTOM METHODS
+     */
+
+    // Used to Flip the Entire Player Sprite and Entities Attaches (From Mini-Quest 2)
+    public void FlipX()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x = theScale.x * -1;
+        transform.localScale = theScale;
+    } // END OF METHOD
+
+} // END OF CLASS 
